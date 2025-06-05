61cf5f36546ce9a96ce6a2e8bb46e56f8e06345c
minibras <damien.eckly@gmail.com>
2025-06-05 16:39:35 +0200

    feat: ajout de l'effet nausea quand consommé

--- src/main/java/com/example/items/ModItems.java
+++ src/main/java/com/example/items/ModItems.java
@@ -2,8 +2,14 @@ package com.example.items;
 
 import com.example.ExampleMod;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.component.type.ConsumableComponent;
+import net.minecraft.component.type.ConsumableComponents;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -14,7 +20,11 @@ import java.util.function.Function;
 
 public class ModItems {
 
-    public static final Item LEMON = register("lemon", Item::new, new Item.Settings());
+    public static final ConsumableComponent NAUSEA_FOOD_CONSUMABLE_COMPONENT = ConsumableComponents.food()
+            // The duration is in ticks, 20 ticks = 1 second
+            .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 6 * 20, 1), 0.5f))
+            .build();
+    public static final Item LEMON = register("lemon", Item::new, new Item.Settings().food(new FoodComponent.Builder().nutrition(1).saturationModifier(1).build(), NAUSEA_FOOD_CONSUMABLE_COMPONENT));
 
     public static void initialize() {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register((itemGroup) -> itemGroup.add(ModItems.LEMON));
